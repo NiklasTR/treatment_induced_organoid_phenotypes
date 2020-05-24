4c9e1c850e84b1df3e87c9aa6cefdf3d6a3db88b
Rindtorff <rindtorf@b110-sc2cn01.inet.dkfz-heidelberg.de>
2020-05-24 17:39:26 +0200

    updated 06_profiling for promise

--- sync_to_cluster.sh
+++ sync_to_cluster.sh
@@ -1,1 +1,1 @@
-rsync -rav /Users/rindtorf/Documents/GitHub/phenotypespectrum/data rindtorf@b110-sc2cn01:/home/rindtorf/github/phenotypespectrum/data 
+rsync -rav /Users/rindtorf/Documents/GitHub/phenotypespectrum/data/ rindtorf@b110-sc2cn01:/home/rindtorf/github/phenotypespectrum/data/ 
