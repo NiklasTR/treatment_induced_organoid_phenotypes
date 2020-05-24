8a45e46a69dc2e513f256b08af0a48acb3e6634c
Niklas <niklastimon@googlemail.com>
2020-05-24 15:54:40 +0200

    essential

---
+++ R/mclust_predict_cluster_umap.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(here)
+library(cluster)
+library(SingleCellExperiment)
+library(mclust)
+
+# load dataset 
+ods <- readRDS(here("data/monocle/bulk_pca_cluster_strat.Rds"))
+
+# diagnose clusters
+set.seed(123456)
+
+df <- reducedDims(ods)$UMAP %>% cbind(colData(ods)) %>% as_tibble()
+df_sample <- df %>% group_by(Line, Replicate) %>% sample_frac(0.1)
+
+print("running mclust")
+clust <- mclust::Mclust(df_sample %>% dplyr::select(V1:V2), 
+                        #modelNames = "EVV", 
+                        G = 1:25,
+                        verbose = TRUE) #5-15 used to be default, tried 1-10
+
+saveRDS(clust, here("data/mclust/umap_mclust.Rds"))
+
+clust_predict <- predict(clust, df %>% dplyr::select(V1:V2))
+
+#saveRDS(clust_predict, here("data/mclust/umap_mclust_predict)
+#saveRDS(clust_predict, here("data/mclust/umap_mclust_predict_k6.Rds"))
+saveRDS(clust_predict, here("data/mclust/umap_mclust_predict.Rds"))
+
+print("done")
---
+++ sync_from_cluster.sh
@@ -0,0 +1,1 @@
+rsync -rav rindtorf@b110-sc2cn01:/home/rindtorf/github/phenotypespectrum/data /Users/rindtorf/Documents/GitHub/phenotypespectrum/data
