4cbe60d15c74bd083abf79f9b9e6a0a7affae636
Niklas <niklastimon@googlemail.com>
2021-07-01 11:03:20 +0200

    adding PROMISE package src

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,14 +5,24 @@ import os
 print('make sure you are running make_dataset on an instance that supports IBM bsub')
 
 # running incremental PCA on data
+<<<<<<< HEAD
+os.system('bsub -R "rusage[mem=150GB]" -q long ./src/models/FeatureAnalysis/run_pca.bsub \
+	-o src/models/FeatureAnalysis/out.bsub \
+	-e src/models/FeatureAnalysis/error.bsub')
+=======
 os.system('bsub -R "rusage[mem=150GB]" -q long ./src/features/FeatureAnalysis/run_pca.bsub \
 	-o src/features/FeatureAnalysis/out.bsub \
 	-e src/features/FeatureAnalysis/error.bsub')
+>>>>>>> f929c08bfcdc5501e94f05f8939c4c516193e13e
 # Aggregated features 6.6M x 486 and incrementalPCA projected features are stored in:  
 # data/interim/FeatureAnalysis/line_differences/human/all_drugs/results
 
 # I am converting the pca transformed object into an .Rds object locally
+<<<<<<< HEAD
+os.system('Rscript src/models/PhenotypeSpectrum/R/tidy_pca.R')
+=======
 os.system('Rscript src/features/tidy_pca.R')
+>>>>>>> f929c08bfcdc5501e94f05f8939c4c516193e13e
 
 # generate umap embedding with or without harmony correction on PCA transformed DMSO-treated organoid features
 os.system('bsub -R "rusage[mem=100GB]" -q long ./src/models/PhenotypeSpectrum/run_umap_dmso.bsub \
