c873d14eb35cc48fc722815b9f1fc435bdd5882d
Niklas <niklastimon@googlemail.com>
2021-07-08 11:16:11 +0200

    debugging D05554 incusion

--- src/models/FeatureAnalysis/PCAScaledFeatures.py
+++ src/models/FeatureAnalysis/PCAScaledFeatures.py
@@ -10,7 +10,12 @@ import CreateOrganoidCutouts
 # list all lines that are currently included within the analysis
 lines = Utils.get_all_lines("human")
 print(lines)
-# 
+
+# list all plates that are currently included
+plates = Utils.get_all_plates("human")
+print(plates)
+
+# run incremental PCA across all included features
 #LineDifferences.process_dmso_organoids_no_filter("human")
 LineDifferences.process_all_organoids("human")
 
