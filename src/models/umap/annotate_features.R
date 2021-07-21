f9be01a474047f668578724a6e213707b7a24734
Niklas <niklastimon@googlemail.com>
2021-07-21 18:28:50 +0200

    refactored feature_intensity to function that can be pointed to source file

--- src/models/umap/annotate_features.R
+++ src/models/umap/annotate_features.R
@@ -8,32 +8,35 @@ library(dplyr)
 library(magrittr)
 library(here)
 
-path = here("data/interim/FeatureAnalysis/line_differences/human/all_drugs/results/filtered_lenient/ReducedFeatures_all_drugs_human.h5")
+annotate_features = function(path){
+  print(path)
+  print('extracting annotation')
 
-print('extracting annotation')
+  h5ls(path)
+  df <- tibble(name = h5read(path, "feature_names_organoids")) %>%
+    mutate(class = case_when(grepl(pattern = "\\.s\\.", x = name) ~ "shape",
+                            grepl(pattern = "\\.b\\.", x = name) ~ "basic",
+                            grepl(pattern = "\\.h\\.", x = name) ~ "haralick",
+                            grepl(pattern = "\\.m\\.", x = name) ~ "moment")) %>%
+    mutate(channel = case_when(grepl(pattern = "x\\.0\\.", x = name) ~ "mask",
+                              grepl(pattern = "x\\.a\\.", x = name) ~ "actin",
+                              grepl(pattern = "x\\.b\\.", x = name) ~ "cell_event",
+                              grepl(pattern = "x\\.c\\.", x = name) ~ "dapi"))
 
-h5ls(path)
-df <- tibble(name = h5read(path, "feature_names_organoids")) %>%
-  mutate(class = case_when(grepl(pattern = "\\.s\\.", x = name) ~ "shape",
-                           grepl(pattern = "\\.b\\.", x = name) ~ "basic",
-                           grepl(pattern = "\\.h\\.", x = name) ~ "haralick",
-                           grepl(pattern = "\\.m\\.", x = name) ~ "moment")) %>%
-  mutate(channel = case_when(grepl(pattern = "x\\.0\\.", x = name) ~ "mask",
-                             grepl(pattern = "x\\.a\\.", x = name) ~ "actin",
-                             grepl(pattern = "x\\.b\\.", x = name) ~ "cell_event",
-                             grepl(pattern = "x\\.c\\.", x = name) ~ "dapi"))
+  # TODO build test to check wether the returned features equal the features listed in Comon_Features_human.txt in that directory
 
-# TODO build test to check wether the returned features equal the features listed in Comon_Features_human.txt in that directory
+  df %>% write_csv(here("data/interim/FeatureAnalysis/feature_annotation.csv"))
 
-df %>% write_csv(here("data/interim/FeatureAnalysis/feature_annotation.csv"))
+  print('reading features')
 
-print('reading features')
+  df <- h5read(path, "features_organoids") %>% as.data.frame() %>% 
+    magrittr::set_colnames(h5read(path, "feature_names_organoids")) %>%
+    dplyr::select(x.a.b.mean, x.b.b.mean, x.c.b.mean)
 
-df <- h5read(path, "features_organoids") %>% as.data.frame() %>% 
-  magrittr::set_colnames(h5read(path, "feature_names_organoids")) %>%
-  dplyr::select(x.a.b.mean, x.b.b.mean, x.c.b.mean)
+  df %>% write_rds(here("data/interim/FeatureAnalysis/feature_intensity.rds"))
 
-df %>% write_rds(here("data/interim/FeatureAnalysis/feature_intensity.rds"))
+  print(head(df))
 
-print(head(df))
+
+}
 
