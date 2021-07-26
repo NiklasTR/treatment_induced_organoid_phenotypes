eb28536020e26aa6353499291de9e9ea7d43b9e7
Niklas <niklastimon@googlemail.com>
2021-07-26 15:27:36 +0200

    updating the tidy function as well

--- src/models/umap/tidy_umap.R
+++ src/models/umap/tidy_umap.R
@@ -4,14 +4,14 @@ print(.libPaths())
 
 # input
 args = commandArgs(trailingOnly=TRUE)
-if (!length(args)==4) {
-  stop("Arguments must be supplied (input file name, seed, sampling_rate, source for feature intensity function).n", call.=FALSE)
+if (!length(args)==5) {
+  stop("Arguments must be supplied (raw input file name, harmony input file name, seed, sampling_rate, source for feature intensity function).n", call.=FALSE)
 } 
 
 print(args)
 
 # seed
-set.seed(args[2])
+set.seed(args[3])
 
 
 # libraries
@@ -37,12 +37,12 @@ PATH = paste0(here::here(), "/")
 source(here::here("src/models/umap/LDC_organoids.R"))
 source(here::here("src/models/umap/annotate_features.R"))
 
-annotate_features(here::here(args[4]))
+annotate_features(here::here(args[5]))
 
 # Loading data and formatting for easier access, commented non-essential code
 obj <- readRDS(paste0(PATH, args[1]))
 # # Also, I load similarly processed data, where only DMSO treated organoids were included in PCA and/or Harmony was tested
-# obj_h <- readRDS(paste0(PATH, "data/interim/PhenotypeSpectrum/harmony_umap_absolute_all_drugs.Rds"))
+obj_h <- readRDS(paste0(PATH, args[2]))
 # dmso_h <- readRDS(paste0(PATH, "data/interim/PhenotypeSpectrum/harmony_umap_absolute_dmso.Rds"))
 # dmso <- readRDS(paste0(PATH, "data/interim/PhenotypeSpectrum/hdf5_umap_absolute_dmso.Rds"))
 
@@ -50,6 +50,7 @@ obj <- readRDS(paste0(PATH, args[1]))
 # clustering UMAP data using graph-based clustering
 length_in = 1e-7
 print(paste0("resolution is ", length_in))
+
 obj = cluster_cells(obj, 
                           reduction_method = "UMAP",
                           cluster_method = "leiden",
@@ -57,15 +58,24 @@ obj = cluster_cells(obj,
                           resolution = length_in,
                           random_seed = 1334)
 
+obj_h = cluster_cells(obj_h, 
+                    reduction_method = "UMAP",
+                    cluster_method = "leiden",
+                    verbose = TRUE, 
+                    resolution = length_in,
+                    random_seed = 1334)
+
 # write object to file
 obj %>% saveRDS(paste0(PATH, args[1]))
+obj_h %>% saveRDS(paste0(PATH, args[2]))
 
 # Formatting UMAP tables for easy access
 umap_tidy <- reducedDims(obj)$UMAP %>% cbind(colData(obj)) %>% as_tibble() %>% janitor::clean_names() 
 pca_tidy <- reducedDims(obj)$PCA %>% cbind(colData(obj)) %>% as_tibble() %>% janitor::clean_names()
 
 # Formatting other UMAP projections
-# umap_tidy_h <- reducedDims(obj_h)$UMAP %>% cbind(colData(obj_h)) %>% as_tibble() %>% janitor::clean_names()
+umap_tidy_h <- reducedDims(obj_h)$UMAP %>% cbind(colData(obj_h)) %>% as_tibble() %>% janitor::clean_names()
+pca_tidy_h <- reducedDims(obj_h)$PCA %>% cbind(colData(obj_h)) %>% as_tibble() %>% janitor::clean_names()
 # ## Spiking in LDC classifier data to DMSO-only data
 # umap_tidy_dmso_h <- reducedDims(dmso_h)$UMAP %>% cbind(colData(dmso_h)) %>% as_tibble() %>% janitor::clean_names()
 # umap_tidy_dmso <- reducedDims(dmso)$UMAP %>% cbind(colData(dmso)) %>% as_tibble() %>% janitor::clean_names()
@@ -77,27 +87,48 @@ intensity <- read_rds(paste0(PATH, "data/interim/FeatureAnalysis/feature_intensi
 umap_tidy <- cbind(umap_tidy, intensity)
 pca_tidy <- cbind(pca_tidy, intensity)
 
+umap_tidy_h <- cbind(umap_tidy_h, intensity)
+pca_tidy_h <- cbind(pca_tidy_h, intensity)
+
 # adding cluster and segment-level data
 umap_tidy <- cbind(umap_tidy, cluster = clusters(obj), partition = partitions(obj)) %>% as_tibble()
 pca_tidy <- cbind(pca_tidy, cluster = clusters(obj), partition = partitions(obj)) %>% as_tibble()
 
+umap_tidy_h <- cbind(umap_tidy_h, cluster = clusters(obj_h), partition = partitions(obj_h)) %>% as_tibble()
+pca_tidy_h <- cbind(pca_tidy_h, cluster = clusters(obj_h), partition = partitions(obj_h)) %>% as_tibble()
 
 # TODO adding LDC values
 
-# chekcking structure
+# checking structure
 print(str(umap_tidy))
 
 # I subsample parts of my data. 
-umap_sampled <- umap_tidy%>%
-  sample_frac(size = as.numeric(args[3]),
+umap_sampled <- umap_tidy %>%
+  sample_frac(size = as.numeric(args[4]),
               replace = FALSE)
 
+pca_sampled <- pca_tidy %>%
+  sample_frac(size = as.numeric(args[4]),
+              replace = FALSE)
+
+umap_sampled_h <- umap_tidy_h %>%
+  sample_frac(size = as.numeric(args[4]),
+              replace = FALSE)
+
+pca_sampled_h <- pca_tidy_h %>%
+  sample_frac(size = as.numeric(args[4]),
+              replace = FALSE)
 # write to file
 umap_tidy %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_tidy.Rds"))
 pca_tidy %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/pca_absolute_all_drugs_tidy.Rds"))
 umap_sampled %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_sampled.Rds"))
+pca_sampled %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/pca_absolute_all_drugs_sampled.Rds"))
 
 # other output data for comparison
-# umap_tidy_h %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/harmony_umap_absolute_all_drugs_tidy.Rds"))
+umap_tidy_h %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/harmony_umap_absolute_all_drugs_tidy.Rds"))
+pca_tidy_h %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/harmony_pca_absolute_all_drugs_tidy.Rds"))
+umap_sampled_h %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/harmony_umap_absolute_all_drugs_sampled.Rds"))
+pca_sampled_h %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/harmony_pca_absolute_all_drugs_sampled.Rds"))
+
 # umap_tidy_dmso_h %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/harmony_umap_absolute_dmso_tidy.Rds"))
 # umap_tidy_dmso %>% write_rds(paste0(PATH, "data/processed/PhenotypeSpectrum/umap_absolute_dmso_tidy.Rds"))
