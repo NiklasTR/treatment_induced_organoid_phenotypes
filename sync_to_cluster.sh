ccd2032094093571ee6c24fb27ce648447d05b7f
Niklas <niklastimon@googlemail.com>
2021-08-13 09:45:26 +0200

    cleaning up repo

--- sync_to_cluster.sh
+++
@@ -1,8 +0,0 @@
-#rsync -rav /Users/rindtorf/Documents/GitHub/promise/data/ rindtorf@b110-sc2cn01:/home/rindtorf/github/promise/data/ 
-rsync -rav /rsession-store/rindtorf/promise rindtorf@b110-sc2cn01:/home/rindtorf/data/promise/github
-
-module load R/3.6.2
-Rscript /rsession-store/rindtorf/promise/make_vignettes.R
-
-find ./* -size +100M | cat >> .gitignore
-find ./* -size +100M | cat >> transfer_file
