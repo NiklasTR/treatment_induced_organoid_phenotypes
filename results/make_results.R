2f470e939f25856a0aac4e624dcbe90ad9332e00
NiklasTR <niklastimon@gmail.com>
2021-08-20 16:37:40 +0300

    running vignette make

---
+++ results/make_results.R
@@ -0,0 +1,15 @@
+# defining lib path
+#.libPaths("/omics/groups/OE0049/b110_data/B110-Isilon2/promise/x86_64-pc-linux-gnu-library/4.0")
+print(.libPaths())
+
+# figure 1
+rmarkdown::render(here::here('notebooks/imaging/organoid_unsupervised_exploration.Rmd'), 
+    params = list(
+        remote = FALSE, 
+        data = "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_tidy.Rds", 
+        sample = "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_tidy_Paclitaxel.Rds",
+        cache = FALSE))
+system("cp ../reports/figures/imaging/* figure_1/")
+
+# figure 2
+# cp ../reports/panels/panel_size_drug.pdf figure_2/
