c008d7bdc5268f236e1d25eff347820c85d95517
Niklas <niklastimon@googlemail.com>
2021-09-01 13:59:46 +0200

    moved

--- src/models/make_data_selection.sh
+++ src/models/make_data_selection.sh
@@ -1,8 +1,8 @@
 # generate subsamples for vizualization
-Rscript --vanilla src/models/umap/select_data_umap.R Paclitaxel 0.1 123
-Rscript --vanilla src/models/umap/select_data_umap.R Bortezomib 0.1 123
-Rscript --vanilla src/models/umap/select_data_umap.R Binimetinib 0.1 123
-Rscript --vanilla src/models/umap/select_data_umap.R Trametinib 0.1 123
-Rscript --vanilla src/models/umap/select_data_umap.R WYE-132 0.1 123
-Rscript --vanilla src/models/umap/select_data_umap.R CHIR-98014 0.1 123
+Rscript --vanilla src/models/umap/select_data_umap.R Paclitaxel 0.3 123
+Rscript --vanilla src/models/umap/select_data_umap.R Bortezomib 0.3 123
+Rscript --vanilla src/models/umap/select_data_umap.R Binimetinib 0.3 123
+Rscript --vanilla src/models/umap/select_data_umap.R Trametinib 0.3 123
+Rscript --vanilla src/models/umap/select_data_umap.R WYE-132 0.3 123
+Rscript --vanilla src/models/umap/select_data_umap.R CHIR-98014 0.3 123
 
