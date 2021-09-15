1fc1367b6534397f4600927b1de9784a4ada0e3d
Niklas <niklastimon@googlemail.com>
2021-09-15 18:19:19 +0200

    reran mofa exploration

--- results/make_results.R
+++ results/make_results.R
@@ -3,25 +3,25 @@
 print(.libPaths())
 
 # figure 1
-rmarkdown::render(here::here('notebooks/imaging/organoid_unsupervised_exploration.Rmd'), 
-    params = list(
-        remote = FALSE, 
-        data = "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_sampled.Rds", 
-        sample = "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_tidy_Paclitaxel.Rds",
-        cache = TRUE))
+# rmarkdown::render(here::here('notebooks/imaging/organoid_unsupervised_exploration.Rmd'), 
+#     params = list(
+#         remote = FALSE, 
+#         data = "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_sampled.Rds", 
+#         sample = "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_tidy_Paclitaxel.Rds",
+#         cache = TRUE))
 
-# figure 2
-rmarkdown::render(here::here('notebooks/imaging/embedding_inspection.Rmd'), 
-    params = list(
-        data = "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_sampled.Rds",
-        data_harmony = "data/processed/PhenotypeSpectrum/harmony_umap_absolute_all_drugs_sampled.Rds",
-        remote = FALSE,
-        cache = TRUE))
-rmarkdown::render(here::here('notebooks/drug_analysis/2.0-js-OrganoidViability.Rmd'))
+# # figure 2
+# rmarkdown::render(here::here('notebooks/imaging/embedding_inspection.Rmd'), 
+#     params = list(
+#         data = "data/processed/PhenotypeSpectrum/umap_absolute_all_drugs_sampled.Rds",
+#         data_harmony = "data/processed/PhenotypeSpectrum/harmony_umap_absolute_all_drugs_sampled.Rds",
+#         remote = FALSE,
+#         cache = TRUE))
+# rmarkdown::render(here::here('notebooks/drug_analysis/2.0-js-OrganoidViability.Rmd'))
 
-# figure 3
-rmarkdown::render(here::here('notebooks/drug_analysis/3.0-js-OrganoidPhenotypes.Rmd'))
+# # figure 3
+# rmarkdown::render(here::here('notebooks/drug_analysis/3.0-js-OrganoidPhenotypes.Rmd'))
 
 # figure 4 and 5
-rmarkdown::render(here::here('notebooks/mofa/mofa_exploration.Rmd'))
+#rmarkdown::render(here::here('notebooks/mofa/mofa_exploration.Rmd'))
 rmarkdown::render(here::here('notebooks/mofa/mofa_drug_effect.Rmd'))
