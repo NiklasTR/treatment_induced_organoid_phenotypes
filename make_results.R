041cf73c5b69b451eca2ed5d89318b015dff6517
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2021-11-15 14:37:12 +0100

    Update make_results.R

--- make_results.R
+++ make_results.R
@@ -32,7 +32,7 @@ rmarkdown::render(here::here('notebooks/drug_activity/2.0-js-OrganoidViability.R
 # requires running the drug_effect scripts within the FeatureAnalysis module
 rmarkdown::render(here::here('notebooks/drug_activity/3.0-js-DrugPhenotypes.Rmd'))
 
-# figure 4 and 5
+# figure 4, 5 and 6
 # running these vignettes requires a trained MOFA model. A MOFA model can be trained from within the MOFA2 docker container
 # by calling the tidy_mofa.R script
 rmarkdown::render(here::here('notebooks/mofa/1.0-nr-mofa_exploration.Rmd'))
