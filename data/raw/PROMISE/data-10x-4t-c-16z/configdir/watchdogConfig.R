861fba29bbeb07dc5157c8b21614c52ee2ae4dd2
NiklasTR <niklastimon@gmail.com>
2021-11-17 14:45:07 +0100

    adding examples for raw data

---
+++ data/raw/PROMISE/data-10x-4t-c-16z/configdir/watchdogConfig.R
@@ -0,0 +1,26 @@
+sleepSeconds=300
+nrWells=384
+indir="/collab-ag-fischer/PROMISE/incoming/PROMISE-10x-4t-c-16z"
+pythondir="/collab-ag-fischer/PROMISE/data-10x-4t-c-16z/configdir/segmentation_config"
+hdf5dir="/collab-ag-fischer/PROMISE/data-10x-4t-c-16z/hdf5dir"
+hdf5validationdir="/collab-ag-fischer/PROMISE/data-10x-4t-c-16z/hdf5validation"
+hdf5projection="/collab-ag-fischer/PROMISE/data-10x-4t-c-16z/hdf5projection"
+htmldir="/collab-ag-fischer/PROMISE/data-10x-4t-c-16z/htmldir"
+layoutdir="/collab-ag-fischer/PROMISE/layouts"
+featuresdir="/collab-ag-fischer/PROMISE/data-10x-4t-c-16z/features"
+segmentationdir="/collab-ag-fischer/PROMISE/data-10x-4t-c-16z/segmentation"
+segmentationhtmldir="/collab-ag-fischer/PROMISE/data-10x-4t-c-16z/segmentationhtmldir"
+tifdiagnosticsdir="/collab-ag-fischer/PROMISE/data-10x-4t-c-16z/tifdiagnostics"
+pythonexec="PROMISE_DNNSegmentation.py"
+qsub="qsub -q ag_fischer -l walltime=24:00:00,select=1:ncpus=1:mem=512MB -W umask=0011"
+startScript="library(PROMISE);startWorkflow();warnings()"
+fctcall="PROMISEconversion"
+# channels=c('dsRed','FITC','DAPI')
+channels=c('dsRed','DAPI')
+# exciteWavelengths=c('Green', 'Blue', 'UV')
+exciteWavelengths=c('Green','UV')
+fields=c('1','2','3','4')
+fields_layout=c('1','2','4','3')
+stacks = c('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16')
+hdf5_compression=3
+hdf5_chunksize=256
